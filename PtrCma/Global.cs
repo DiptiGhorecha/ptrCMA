a3ec616a83e35fc17bb2c7c75ebeee63c88613be
Dipti <dipti.gorecha@prodigyinfosoft.com>
2017-11-28 14:14:57 +0530

    Fixed resources problem

--- PtrCma/Global.cs
+++ PtrCma/Global.cs
@@ -3,24 +3,27 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace PtrCma
 {
     public static class Global
     {
-        //Main Page Form
+        //MDI Form
         public static string companyName = "Perfect Tax Reporter - CMA 1.0";
         public static Color appBackColr = System.Drawing.Color.LightSeaGreen;
-        public static Image startFrmBackImg = Image.FromFile("Resources\\bg.png"); //Backgrounf Image release when form is load
+        
+        //MAIN form
+        public static Bitmap startFrmBackImg = new Bitmap(Assembly.GetExecutingAssembly().GetManifestResourceStream("PtrCma.Resources.bg.png"));
         public static int cmdBtnWidthMain = 200;
         public static int cmdBtnHeightMain = 68;
-        public static Image cmdBnkStndIcon = Image.FromFile("Resources\\icon1.png");
-        public static Image cmdCmsProjIcon = Image.FromFile("Resources\\icon2.png");
-        public static Image cmdBackupIcon = Image.FromFile("Resources\\icon3.png");
-        public static Image cmdRestoreIcon = Image.FromFile("Resources\\icon4.png");
-        public static Image cmdExitIcon = Image.FromFile("Resources\\icon5.png");
-        public static Image cmdBtnBack = Image.FromFile("Resources\\button1.png");
+        public static Bitmap cmdBnkStndIcon = new Bitmap(Assembly.GetExecutingAssembly().GetManifestResourceStream("PtrCma.Resources.icon1.png"));    
+        public static Bitmap cmdCmsProjIcon = new Bitmap(Assembly.GetExecutingAssembly().GetManifestResourceStream("PtrCma.Resources.icon2.png"));   
+        public static Bitmap cmdBackupIcon = new Bitmap(Assembly.GetExecutingAssembly().GetManifestResourceStream("PtrCma.Resources.icon3.png"));      
+        public static Bitmap cmdRestoreIcon = new Bitmap(Assembly.GetExecutingAssembly().GetManifestResourceStream("PtrCma.Resources.icon4.png"));   
+        public static Bitmap cmdExitIcon = new Bitmap(Assembly.GetExecutingAssembly().GetManifestResourceStream("PtrCma.Resources.icon5.png"));     
+        public static Bitmap cmdBtnBack = new Bitmap(Assembly.GetExecutingAssembly().GetManifestResourceStream("PtrCma.Resources.button1.png"));    
         public static Font cmdBtnfont = new System.Drawing.Font("Arial", 9.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
         public static Color cmdFontColr = System.Drawing.Color.White;
         public static Font lblCMSfont = new System.Drawing.Font("Viner Hand ITC", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
